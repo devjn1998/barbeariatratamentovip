12be76f79222576e24e65c1741925609b1cacdb9
devjn188 <joiasimporio@gmail.com>
2025-04-14 20:04:45 -0300

    adicionando novos preços

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -73,8 +73,8 @@ export default function HomePage() {
         "Corte, pintura e sobrancelha com produtos de alta qualidade para garantir um visual duradouro e elegante.",
     },
     {
-      name: "Combo Cabelo+Barba+Sobrancelha",
-      price: "R$ 50",
+      name: "Combo Cabelo+Sobrancelha",
+      price: "R$ 40.00",
       image:
         "https://images.unsplash.com/photo-1599351431202-1e0f0137899a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=80",
       description: "Experiência premium com produtos exclusivos",
