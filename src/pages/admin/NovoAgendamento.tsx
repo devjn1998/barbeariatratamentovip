08db82e224af2a9a76a66cd39160f4e26fe6cfa7
devjn188 <joiasimporio@gmail.com>
2025-04-21 21:25:48 -0300

    adicionando funcionalidade de add agendamentos #3

--- src/pages/admin/NovoAgendamento.tsx
+++ src/pages/admin/NovoAgendamento.tsx
@@ -317,10 +317,7 @@ export default function NovoAgendamento() {
             className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-amber-500 focus:border-amber-500 bg-white"
           >
             {/* Adicione os status relevantes que você usa */}
-            <option value="agendado">Agendado</option>
             <option value="confirmado">Confirmado</option>
-            <option value="concluido">Concluído</option>
-            <option value="cancelado">Cancelado</option>
             <option value="aguardando pagamento">Aguardando Pagamento</option>
           </select>
         </div>
